f75f2497a454b2c961f0daff3899ea413aea234f
Claire <claire.williams@thebookpeople.co.uk>
2019-07-03 22:13:04 +0100

    initial commit with ability to pass folder to watch on the command line & watch it, report changes to contents in console at the moment

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+  "fmt"
+  "log"
+  "os"
+  "time"
+  "github.com/radovskyb/watcher"
+)
+
+func main() {
+  watchFolderPath := os.Args[1]
+  // watchFolderPath := "/Users/clairew/watch_me/2019/March"
+
+  w := watcher.New()
+
+	// SetMaxEvents to 1 to allow at most 1 event's to be received
+	// on the Event channel per watching cycle.
+	//
+	// If SetMaxEvents is not set, the default is to send all events.
+	w.SetMaxEvents(1)
+
+	// the events we want to be notified of
+	w.FilterOps(watcher.Rename, watcher.Move, watcher.Create, watcher.Remove)
+
+  go func() {
+		for {
+			select {
+			case event := <-w.Event:
+				fmt.Printf("here %v", event) // Print the event's info.
+			case err := <-w.Error:
+				log.Fatalln(err)
+			case <-w.Closed:
+				return
+			}
+		}
+	}()
+
+	// Watch the given folder for changes
+	if err := w.Add(watchFolderPath); err != nil {
+		log.Fatalln(err)
+	}
+
+	// Print a list of all of the files and folders currently
+	// being watched and their paths.
+	for path, f := range w.WatchedFiles() {
+		fmt.Printf("%s: %s\n", path, f.Name())
+	}
+
+	fmt.Println()
+
+	// Start the watching process - it'll check for changes every 100ms.
+	if err := w.Start(time.Millisecond * 100); err != nil {
+		log.Fatalln(err)
+	}
+}
